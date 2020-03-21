f100f09d69c91f526e3dc5f386883793b9d0dfda
Victor Künstler <victor.kuenstler@outlook.com>
2020-03-21 18:28:11 +0100

    Pushie Pushpush

--- frontend/views/Welcome.js
+++ frontend/views/Welcome.js
@@ -3,15 +3,18 @@ import { Layout, Button, Text } from '@ui-kitten/components';
 
 const WelcomeScreen = ({ navigation }) => (
   <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-    <Text category="h1">Wer bist du?</Text>
+    <Text category="h1" sytle={{ margin: 25 }}>Wer bist du?</Text>
     <Button
       onPress={() => navigation.navigate('Landwirt', { name: 'Jane' })}
+      size="giant"
     >
       Landwirt
     </Button>
     <Button
       title="Helfer"
       onPress={() => navigation.navigate('Map', { name: 'lulz' })}
+      style={{ margin: 24 }}
+      size="giant"
     >
       Helfer
     </Button>
--- frontend/views/WelcomeLandwirt.js
+++ frontend/views/WelcomeLandwirt.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import {
-  View, Button, TextInput
-} from 'react-native';
-import DatePicker from 'react-native-datepicker';
+  RangeCalendar, Layout, Input, Button
+} from '@ui-kitten/components';
 
 class WelcomeLandwirt extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      start_date: new Date.prototype.getDate(),
-      end_date: '2016-05-15',
+      range: {
+        startDate: null,
+        endDate: null,
+      },
       name: 'Der Doedel',
       plz: 72766
     };
@@ -22,65 +23,23 @@ class WelcomeLandwirt extends React.Component {
 
   render() {
     return (
-      <View>
-        <DatePicker
-          style={{ width: 200 }}
-          date={this.state.start_date}
-          mode="date"
-          placeholder="select starting date"
-          format="YYYY-MM-DD"
-          minDate="2016-05-01"
-          maxDate="2016-06-01"
-          confirmBtnText="Confirm"
-          cancelBtnText="Cancel"
-          customStyles={{
-            dateIcon: {
-              position: 'absolute',
-              left: 0,
-              top: 4,
-              marginLeft: 0
-            },
-            dateInput: {
-              marginLeft: 36
-            }
-          }}
-          onDateChange={(date) => { this.setState({ start_date: date }); }}
+      <Layout>
+        <RangeCalendar
+          range={this.state.range}
+          onSelect={(range) => this.setState(range)}
         />
-        <DatePicker
-          style={{ width: 200 }}
-          date={this.state.end_date}
-          mode="date"
-          placeholder="select ending date"
-          format="YYYY-MM-DD"
-          minDate="2016-05-01"
-          maxDate="2016-06-01"
-          confirmBtnText="Confirm"
-          cancelBtnText="Cancel"
-          customStyles={{
-            dateIcon: {
-              position: 'absolute',
-              left: 0,
-              top: 4,
-              marginLeft: 0
-            },
-            dateInput: {
-              marginLeft: 36
-            }
-          }}
-          onDateChange={(date) => { this.setState({ end_date: date }); }}
-        />
-        <TextInput
-          onChangeText={(name) => this.setState({ name })}
+        <Input
+          placeholder="Place your Text"
           value={this.state.name}
-          style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+          onChangeText={(name) => this.setState({ name })}
         />
-        <TextInput
+        <Input
+          placeholder="Place your Text"
+          value={this.state.name}
           onChangeText={(plz) => this.setState({ plz })}
-          value={this.state.plz}
-          style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
         />
         <Button onPress={() => this.onHandleSubmit()} title="Submit" />
-      </View>
+      </Layout>
     );
   }
 }
