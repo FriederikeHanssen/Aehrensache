85079d1f519967dc0bd8e02d2ae900689f783bba
Samuel <wunderlich.samuel2@gmai.com>
2020-03-21 16:27:12 +0100

    Auto stash before checking out "HEAD"
    WIP Landwirt from

--- frontend/views/WelcomeLandwirt.js
+++ frontend/views/WelcomeLandwirt.js
@@ -1,11 +1,71 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import {
+  View, Button, TextInput
+} from 'react-native';
+import { Formik } from 'formik';
+import DatePicker from 'react-native-datepicker';
 
-const WelcomeLandwirt = () => (
-  <View>
-    <Text h1>Hello Lanwirt</Text>
-  </View>
-);
+class WelcomeLandwirt extends React.Component {
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      date_picker_value : "2016-05-15",
+    };
+  }
+
+  render() {
+    return (
+      <View>
+        <DatePicker
+          style={{ width: 200 }}
+          date={this.state.date_picker_value}
+          mode="date"
+          placeholder="select date"
+          format="YYYY-MM-DD"
+          minDate="2016-05-01"
+          maxDate="2016-06-01"
+          confirmBtnText="Confirm"
+          cancelBtnText="Cancel"
+          customStyles={{
+            dateIcon: {
+              position: 'absolute',
+              left: 0,
+              top: 4,
+              marginLeft: 0
+            },
+            dateInput: {
+              marginLeft: 36
+            }}}
+            // ... You can check the source to find the other keys.
+            onDateChange={(date) => this.setState({ date_picker_value: date }})}
+        />
+        <Formik
+          initialValues={{ name: '' }}
+          onSubmit={(values, actions) => {
+            alert(JSON.stringify(values));
+            setTimeout(() => {
+              actions.setSubmitting(false);
+            }, 1000);
+          }}
+        >
+          {formikProps => (
+            <React.Fragment>
+              <TextInput
+                onChangeText={formikProps.handleChange('name')}
+              />
+              {formikProps.isSubmitting ? (
+                <ActivityIndicator />
+              ) : (
+                <Button title="Submit" onPress={formikProps.handleSubmit} />
+              )}
+            </React.Fragment>
+          )}
+        </Formik>
+      </View>
+
+    );
+  }
+}
 
 export default WelcomeLandwirt;
