1a0ea63ff58b530dfe201f3800306d1e4bbbe60d
Victor Künstler <victor.kuenstler@outlook.com>
2020-03-21 11:53:49 +0100

    Add eslint

--- frontend/App.js
+++ frontend/App.js
@@ -1,22 +1,21 @@
-import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import {
+  Platform, StyleSheet, Text, View
+} from 'react-native';
 
 const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' + 'Shake or press menu button for dev menu',
+  ios: 'Press Cmd+R to reload,\n Cmd+D or shake for dev menu',
+  android: 'Double tap R on your keyboard to reload,\n Shake or press menu button for dev menu',
 });
+const App = () => (
+  <View style={styles.container}>
+    <Text style={styles.welcome}>Welcome to React Native!</Text>
+    <Text style={styles.instructions}>To get started, edit App.js</Text>
+    <Text style={styles.instructions}>{instructions}</Text>
+  </View>
+);
 
-export default class App extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
-  }
-}
+export default App;
 
 const styles = StyleSheet.create({
   container: {
