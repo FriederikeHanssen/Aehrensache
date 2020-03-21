9d9161a051dc589c1e6e11d2756763f51ca41b94
Denis <denis.heid@protonmail.com>
2020-03-21 13:28:42 +0100

    Marker tests

--- frontend/views/Map.js
+++ frontend/views/Map.js
@@ -1,26 +1,31 @@
 import React from 'react';
-import { Marker } from MapView from 'react-native-maps';
+import { Marker, PROVIDER_GOOGLE } from MapView from 'react-native-maps';
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import mapsStyle from '../maps_styling.json';
 
 
 
-export default class App extends React.Component {
-  state = {
-    region: {
-      latitude: 37.78825,
-      longitude: -122.4324,
-      latitudeDelta: 0.0922,
-      longitudeDelta: 0.0421,
-    },
-    markers: [
-      {
-        latlng:
-          [37.7882
-            - 122.4324],
-        title: 'Testmarker',
-        description: 'dude'
-      }
-    ]
+export default class Map extends React.Component {
+  constructor(props) {
+    super(props);
+    this.props.mapsStyle = mapsStyle;
+    this.setState({
+      region: {
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      },
+      markers: [
+        {
+          latlng:
+            [37.7882
+              - 122.4324],
+          title: 'Testmarker',
+          description: 'dude'
+        }
+      ]
+    });
   }
 
   onRegionChange(region) {
@@ -33,6 +38,7 @@ export default class App extends React.Component {
 
         <MapView
           style={styles.map}
+          provider={PROVIDER_GOOGLE}
           initialRegion={{
             latitude: 37.78825,
             longitude: -122.4324,
@@ -67,9 +73,13 @@ const styles = StyleSheet.create({
   },
   map: {
     position: 'absolute',
-    top: 0,
+    top: -10,
     left: 0,
     right: 0,
     bottom: 0,
   }
 });
+const mapStyle = [
+  { "elementType": "geometry", "stylers": [{ "color": "#f5f5f5" }] },
+  { "elementType": "labels.icon", "stylers": [{ "visibility": "off" }] },
+];
